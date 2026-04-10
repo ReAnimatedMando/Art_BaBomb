e1c162693c88e1ec34a26d0a00e9d97cdf641018
Aaron Tanguma <the_guma@yahoo.com>
2026-04-10 12:15:51 -0600

    moved javascript into root to clean up details.cshtml

--- obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35621a5e99fece0a0208bcf35ef3c2ac0ebef081")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc067f78c1749ccf11386b8febbb274f01c32b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
