7b36664387cc3d9e436204d7ce423eca7b6515b2
Aaron Tanguma <the_guma@yahoo.com>
2026-04-20 19:41:29 -0600

    In items/Details, got rid of redundant receipt view.

--- obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9575b8d1430bfe43fd686ca69df6f00b898c5343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22e62e0617c2e16d922b37bd6fbf3d4ed3999e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
