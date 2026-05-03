f0f2cbbdd03d069b001de272629a9a6a36a4a26c
Aaron Tanguma <the_guma@yahoo.com>
2026-05-03 12:02:46 -0600

    Styles upgrade applied to Project/Details.cshtml

--- obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a155ab5cbb4e2952f49fa3a185da541f6d2c782a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a11e1f54caa6e031eca467fbb572f48c6c93436")]
 [assembly: System.Reflection.AssemblyProductAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
