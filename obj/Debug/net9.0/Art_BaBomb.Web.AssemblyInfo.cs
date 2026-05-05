32f33e6ccf19c1cfb337e80b51ef185b1dfe2c01
Aaron Tanguma <the_guma@yahoo.com>
2026-05-05 12:08:44 -0600

    edited spacing and continuity on all app pages

--- obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b824426a56efeeeff5dccedaf68e1837459164b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa05a395cc0452b67e4bd15b3c338bac0c534540")]
 [assembly: System.Reflection.AssemblyProductAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
