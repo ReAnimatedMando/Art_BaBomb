00b6a01fc03293e19e8e2e93996b5ff57f331d27
Aaron Tanguma <the_guma@yahoo.com>
2026-05-06 15:21:13 -0600

    Created test databases

--- obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Art_BaBomb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51090e32c126cffc751ae2ace6d6f2ffa3cb00fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98ed1f54591bfb19f74f3dcb9b13af7884ae856")]
 [assembly: System.Reflection.AssemblyProductAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Art_BaBomb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
